4ff5bf5fa7b8b1c6532f9ecf87b36cc95f7a209d
Nevin Allwood <nevinallwood@gmail.com>
2017-12-12 22:34:08 +0100

    debugged robust script

--- script.py
+++ script.py
@@ -4,16 +4,21 @@ import sys
 import subprocess
 import os
 import signal
+import smtplib
 from time import gmtime,strftime
+from email.mime.text import MIMEText
+
 
 if len(sys.argv) < 2:
     sys.exit("please define TARGET.\n eg.: python script.py sky\n")
 
 platform = sys.argv[1]
 
-DIRECTORY_PATH = os.path.join(os.pardir,'Measurements/{}'.format(platform))
+DIRECTORY_PATH = os.path.join('/media/nevin/D29077E29077CB8B/Measurements/{}'.format(platform))
 if not os.path.exists(DIRECTORY_PATH):
-    os.makedirs(DIRECTORY_PATH)
+    DIRECTORY_PATH = os.path.join(os.pardir,'Measurements/{}'.format(platform))
+    if not os.path.exists(DIRECTORY_PATH):
+        os.makedirs(DIRECTORY_PATH)
 
 # load config
 with open(os.path.join(DIRECTORY_PATH,'config.json')) as config_file:
@@ -30,9 +35,23 @@ broken_lines_counter = 0
 same_round_counter = 0
 last_round = -1
 
-# handles Ctrl+C termination
+#sends mail to emails specified in the config
+def sendMail(message):
+    msg = MIMEText(message)
+    msg['Subject'] = 'Experiment Status'
+    msg['From'] = 'DoorMonitoringSystem@gmail.com'
+    msg['To'] = "nevin.allwood@yahoo.de"
+
+    s = smtplib.SMTP_SSL('smtp.gmail.com',465)
+    s.login("DoorMonitoringSystem@gmail.com","WSN-Project17")
+    s.sendmail("DoorMonitoringSystem@gmail.com",to,msg.as_string())
+    s.quit()
+
+#handles Ctrl+C termination
 def signal_handler(signum,frame):
     print(">exiting process")
+    for process in subprocesses:
+        process.kill()
     sys.exit(0)
 
 #only works for single sink setup as of now
@@ -43,39 +62,45 @@ def reboot_sink():
     while(get_untagged_input != 'Booted\n'):
         print("...")
         write_to_subprocesses("reboot\n")
-        sys.sleep(0.5) #to give device time to reboot
+        time.sleep(0.5) #to give device time to reboot
 
     print(">Sink rebooted")
 
 #checks if the input is script relevant by splitting at '$' and returning the split part
 def get_untagged_input():
+    global broken_lines_counter
+
     for process in subprocesses:
         line = process.stdout.readline()
         if line.startswith('NODE$'):
             return line.split('$')[1]
         elif '$' in line:
-            print(">line broken")
-            broken_lines += 1
+            sys.stdout.write(">line broken: "+line)
+            broken_lines_counter += 1
             return ""
         else:
             return ""
 
+#sends a string to all registered subprocesses
 def write_to_subprocesses(str):
     for process in subprocesses:
         process.stdin.write(str)
 
+#prints round/saves measurement/verifies round depending on input line
 def handle_line(line):
     global checklist
     global round_failed
     global current_round
     global recently_reset
-    global broken_lines
+    global broken_lines_counter
+    global last_round
+    global same_round_counter
 
     if line == "":
         return
 
-    if line.startswith('Round=') and len(line) < 10:     #additional check to make script more robust in case lines are broken
-        broken_lines = 0;
+    if line.startswith('Round=') and len(line) < 11:     #additional check to make script more robust in case lines are broken
+        broken_lines_counter = 0;
         sys.stdout.write(line)
         current_round = int(line.split('=')[1].rstrip())
         if current_round == last_round:
@@ -87,7 +112,7 @@ def handle_line(line):
     #bundle link data from a node
     elif ':' in line:
         if len(line.split(':')) is 6:
-            broken_lines = 0
+            broken_lines_counter = 0
             now = time.time()
             measurement = {}
             node_id = line.split(':')[0]
@@ -110,13 +135,12 @@ def handle_line(line):
                 with open(os.path.join(DIRECTORY_PATH,filename),'a') as f:
                     f.write(str(measurement)+'\n')
         else:
-            sys.stdout.write(">line broken\n")
-            boken_lines += 1
+            sys.stdout.write(">line broken: "+line)
+            broken_lines_counter += 1
 
 
     elif line == 'round finished\n':
-        print("Round {} finished".format(str(current_round)))
-        broken_lines = 0
+        broken_lines_counter = 0
         round_failed = False
         #initial round or rounds after reset only complete if all nodes report back, so checklist has to be empty
         if (current_round == 0 or recently_reset) and not checklist:
@@ -132,19 +156,20 @@ def handle_line(line):
         round_failed = True
 
     elif line == 'reset\n':
-        broken_lines = 0
+        broken_lines_counter = 0
         sys.stdout.write(">All nodes must report back again\n")
         checklist = range(1,number_of_nodes+1)
         recently_reset = True
 
     elif line == 'measurement complete\n':
-        broken_lines = 0
+        broken_lines_counter = 0
         return
 
     else:
-        sys.stdout.write(">line broken\n")
-        boken_lines +=1
+        sys.stdout.write(">line broken: "+line)
+        broken_lines_counter +=1
 
+#throws the debugger board of the sensortags out of the devices list in case it is present
 def throw_out_debugger():
     highest = 0
     for device in devices:
@@ -153,6 +178,7 @@ def throw_out_debugger():
     if highest != 0:
         devices.remove('ttyACM'+str(highest))
 
+#goes through devices list andstarts a bash subprocess. then performs login
 def subprocess_init():
     for device in devices:
         process = subprocess.Popen(['/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -170,6 +196,7 @@ subprocess_init()
 signal.signal(signal.SIGINT, signal_handler)
 
 #loop through configs and start described experiments
+#sendMail("Expermient with {} started".format(platform))
 experimentstart = time.time()
 for config in configurations:
     number_of_nodes = int(config[0])
@@ -187,7 +214,7 @@ for config in configurations:
     handle_line(line);
     while line != 'measurement complete\n':
         #if 4 lines in a row couldn't be read because they are broken
-        if broken_lines > 4:
+        if broken_lines_counter > 4:
             print(">broken lines reset.")
             print(">last config was:"+config)
             reboot_sink()
@@ -197,11 +224,11 @@ for config in configurations:
             write_to_subprocesses(config+"\n")
 
         #if the same round is being send more than 12 times either channel or tx power isn't working, so skip measurement next time sink is waiting for validation
-        if same_round_counter > 12
+        if same_round_counter > 12:
             print(">Skipping this config")
             reboot_sink()
             break
-        
+
         line = get_untagged_input()
         handle_line(line)
 
@@ -210,6 +237,6 @@ for config in configurations:
     elapsed_time = time.time() -starttime
     print(">"+strftime("%H:%M:%S",gmtime(elapsed_time)))
 
-
+#sendMail(">Experiment with {} took: ".format(platform) + strftime("%H:%M:%S",gmtime(elapsed_time)))
 sys.stdout.write(">Finished\n")
 print(">Experiment took: "+strftime("%H:%M:%S",gmtime(elapsed_time)))
